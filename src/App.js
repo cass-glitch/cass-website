a5e7643c52d31e53726b2ee306892752bb7870b5
Tim Seifert <timothy.seifert@tufts.edu>
2020-07-16 21:48:00 -0400

    gh-pages still no work

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import Parallaxe from './Parallaxe.js';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
 
 class App extends Component {
 render() {    
   return (
+       <React.Fragment>
+      <Nav.Link href="/personal-website/#/Parralaxe"></Nav.Link>
       <Router>
                <Switch>
-               <Route exact path= "/" component={Parallaxe} />
+               <Route exact path= "/Parallaxe" component={Parallaxe} />
                </Switch>
      </Router>
+ </React.Fragment>
     
   );
 }
