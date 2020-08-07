0679fa33ffd1bce885dcd59cea6a6ced6655af34
Tim Seifert <timothy.seifert@tufts.edu>
2020-08-07 12:48:10 -0400

    making changes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React,  {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Parallaxe from './Parallaxe.js';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { Nav, Navbar } from 'react-bootstrap';
 
 class App extends Component {
 render() {    
