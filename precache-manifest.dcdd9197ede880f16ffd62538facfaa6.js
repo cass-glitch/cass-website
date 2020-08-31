db5d30143d09695c656c4337c27cef95ebc79e63
Tim Seifert <timothy.seifert@tufts.edu>
2020-08-31 15:27:54 -0400

    Updates

---
+++ precache-manifest.dcdd9197ede880f16ffd62538facfaa6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44906abd8a15114bd1ee61e7f2b6a511",
+    "url": "/cass-website/index.html"
+  },
+  {
+    "revision": "8eb8b4bb94e6aff84e64",
+    "url": "/cass-website/static/css/main.3849bb78.chunk.css"
+  },
+  {
+    "revision": "f04260380b899d83e27c",
+    "url": "/cass-website/static/js/2.4cb4b4b9.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/cass-website/static/js/2.4cb4b4b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb8b4bb94e6aff84e64",
+    "url": "/cass-website/static/js/main.08bba871.chunk.js"
+  },
+  {
+    "revision": "e77fdc9e374dd44532d2",
+    "url": "/cass-website/static/js/runtime-main.0ad2859d.js"
+  },
+  {
+    "revision": "a346f9973014960de6ece5075b0df38f",
+    "url": "/cass-website/static/media/Best_ChalkBoard.a346f997.jpg"
+  },
+  {
+    "revision": "dd721d24f544eb0ff7e37f56b5ee94f6",
+    "url": "/cass-website/static/media/Graduation.dd721d24.jpg"
+  },
+  {
+    "revision": "17ff2ae0a95d2436834b3125b5d1415b",
+    "url": "/cass-website/static/media/Helping.17ff2ae0.jpg"
+  },
+  {
+    "revision": "a64dfd8fcb55bac4ec04df88e27ea175",
+    "url": "/cass-website/static/media/JHU1.a64dfd8f.jpg"
+  },
+  {
+    "revision": "285b1991575c7677907fe88b88ced9b4",
+    "url": "/cass-website/static/media/JHU2.285b1991.jpg"
+  },
+  {
+    "revision": "2514efdbc44599005c5d278b7071e30a",
+    "url": "/cass-website/static/media/JHU3.2514efdb.JPG"
+  },
+  {
+    "revision": "52828b3ae849d116f6347e209b383e58",
+    "url": "/cass-website/static/media/Photo1.52828b3a.jpg"
+  },
+  {
+    "revision": "82e54e96c3039e05ea55c49d6bcb8d93",
+    "url": "/cass-website/static/media/Photo2.82e54e96.jpg"
+  },
+  {
+    "revision": "c3225b167528eed9150e93d733600b63",
+    "url": "/cass-website/static/media/Photo3.c3225b16.jpg"
+  },
+  {
+    "revision": "544e1499ae6e02c8c188239f06aae6e3",
+    "url": "/cass-website/static/media/Photo4.544e1499.jpg"
+  },
+  {
+    "revision": "b29fb75f0db3f39f4650197a6e26ddae",
+    "url": "/cass-website/static/media/Photo7.b29fb75f.jpg"
+  },
+  {
+    "revision": "7126bcb7181c770001f8597c634be14b",
+    "url": "/cass-website/static/media/Photo8.7126bcb7.jpg"
+  },
+  {
+    "revision": "3c581fc4b0d31a4819c0f6e56faf6ef6",
+    "url": "/cass-website/static/media/Photo9.3c581fc4.jpg"
+  },
+  {
+    "revision": "e6d93fbf5002ba435011ff33de7f2d3a",
+    "url": "/cass-website/static/media/Scotty1.e6d93fbf.jpg"
+  },
+  {
+    "revision": "65106bb8e3c8176f86bb25dfb8a1d021",
+    "url": "/cass-website/static/media/Scotty3.65106bb8.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cass-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
